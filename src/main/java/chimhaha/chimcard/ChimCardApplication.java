32873fdbf8eda72116c78e10e6e6aff70870421c
DrumJ <tmdgh717@naver.com>
2024-11-29 15:40:11 +0900

    프로젝트 생성

---
+++ src/main/java/chimhaha/chimcard/ChimCardApplication.java
@@ -0,0 +1,13 @@
+package chimhaha.chimcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChimCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChimCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chimhaha/chimcard/ChimCardApplicationTests.java
@@ -0,0 +1,13 @@
+package chimhaha.chimcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChimCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
