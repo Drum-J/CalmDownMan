28fb7b3fdacbe8590308a21a8fb5aeb7bcd77cfc
DrumJ <tmdgh717@naver.com>
2025-04-28 13:31:12 +0900

    refactor. 카드 공격 타입 Enum 한글 필드 추가

--- src/main/java/chimhaha/chimcard/entity/AttackType.java
+++ src/main/java/chimhaha/chimcard/entity/AttackType.java
@@ -1,5 +1,22 @@
 package chimhaha.chimcard.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum AttackType {
-    ROCK, SCISSORS, PAPER, ALL
+    ROCK("묵"), SCISSORS("찌"), PAPER("빠"), ALL("ALL");
+
+    private final String type;
+
+    public static AttackType getEnum(String type) {
+        for (AttackType attackType : values()) {
+            if (type.equals(attackType.type)) {
+                return attackType;
+            }
+        }
+
+        return null;
+    }
 }
