0c60685b117f0800266ffa4ed5b7e3b7d0868fd3
DrumJ <tmdgh717@naver.com>
2025-05-08 13:36:43 +0900

    feat. 커스텀 Exception 생성 및 글로벌 예외처리 추가

--- src/main/java/chimhaha/chimcard/exception/GlobalExceptionHandler.java
+++ src/main/java/chimhaha/chimcard/exception/GlobalExceptionHandler.java
@@ -1,16 +1,29 @@
 package chimhaha.chimcard.exception;
 
 import chimhaha.chimcard.common.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ApiResponse<String>> notFound(IllegalArgumentException e) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<String>> notFound(ResourceNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.notFound(e.getMessage()));
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiResponse<String>> badRequest(IllegalArgumentException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.badRequest(e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<String>> internalServerError(Exception e) {
+        log.error("Internal Server Error: ", e);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.error(e.getMessage()));
     }
 }
