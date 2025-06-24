b7022ed55d369b5dac23bdcc0ff7b19f1549b950
DrumJ <tmdgh717@naver.com>
2025-06-24 16:54:48 +0900

    refactor. Grade, TradeStatus Enum 수정

--- src/main/java/chimhaha/chimcard/entity/Grade.java
+++ src/main/java/chimhaha/chimcard/entity/Grade.java
@@ -1,5 +1,21 @@
 package chimhaha.chimcard.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Grade {
-    SSR , SR , R , N , C , V
+    SSR(0) , SR(1) , R(2) , N(3) , C(4) , V(5);
+
+    private final int value;
+
+    public static Grade getEnum(int value) {
+        for (Grade grade : values()) {
+            if (value == grade.value) {
+                return grade;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/chimhaha/chimcard/entity/TradeStatus.java
+++ src/main/java/chimhaha/chimcard/entity/TradeStatus.java
@@ -1,5 +1,21 @@
 package chimhaha.chimcard.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TradeStatus {
-    WAITING, COMPLETED, REJECTED, CANCEL
+    WAITING("대기"), COMPLETED("완료"), REJECTED("거절"), CANCEL("취소");
+
+    private final String status;
+
+    public static TradeStatus getEnum(String status) {
+        for (TradeStatus tradeStatus : values()) {
+            if (status != null && status.equals(tradeStatus.status)) {
+                return tradeStatus;
+            }
+        }
+        return null;
+    }
 }
