41efad7a40ba1f849ff0ed3a3faf4481d7c11c29
DrumJ <tmdgh717@naver.com>
2025-07-21 09:45:42 +0900

    feat. 게임 매칭 성공 시 상대 플레이어 닉네임 및 내 게임카드 조회 API

--- src/main/java/chimhaha/chimcard/game/controller/GameController.java
+++ src/main/java/chimhaha/chimcard/game/controller/GameController.java
@@ -1,18 +1,29 @@
 package chimhaha.chimcard.game.controller;
 
+import chimhaha.chimcard.common.ApiResponse;
 import chimhaha.chimcard.game.dto.CardSubmitRequestDto;
+import chimhaha.chimcard.game.dto.GameInfoDto;
 import chimhaha.chimcard.game.service.GameService;
 import chimhaha.chimcard.utils.AccountUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class GameController {
 
     private final GameService gameService;
+
+    @GetMapping("/api/game/{gameRoomId}")
+    public ApiResponse<GameInfoDto> gameInfo(@PathVariable("gameRoomId") Long gameRoomId) {
+        Long playerId = AccountUtils.getAccountId();
+
+        return ApiResponse.success(gameService.gameInfo(gameRoomId, playerId));
+    }
 
     /**
      * 카드 제출 요청을 처리합니다.
