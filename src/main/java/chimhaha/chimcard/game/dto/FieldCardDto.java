dcd2d1cb658c1a289bd21db8fdb1a5271504d548
DrumJ <tmdgh717@naver.com>
2025-07-24 15:00:51 +0900

    refactor. 필드 카드 DTO 데이터 추가 및 AttackType 반환값 변경

--- src/main/java/chimhaha/chimcard/game/dto/FieldCardDto.java
+++ src/main/java/chimhaha/chimcard/game/dto/FieldCardDto.java
@@ -1,12 +1,20 @@
 package chimhaha.chimcard.game.dto;
 
 import chimhaha.chimcard.entity.GameCard;
+import chimhaha.chimcard.entity.Grade;
 
 public record FieldCardDto(Long gameCardId, String imageUrl,
+                           Grade grade,
+                           int power,
+                           String attackType,
                            boolean isFront, // true: 앞면, false: 뒷면
                            boolean isMine // true: 내 카드, false: 상대 카드
 ) {
     public FieldCardDto(GameCard gameCard, boolean isMine) {
-        this(gameCard.getId(), gameCard.getCard().getImageUrl(), gameCard.isFront(), isMine);
+        this(gameCard.getId(), gameCard.getCard().getImageUrl(),
+                gameCard.getCard().getGrade(),
+                gameCard.getCard().getPower(),
+                gameCard.getCard().getAttackType().getType(),
+                gameCard.isFront(), isMine);
     }
 }
--- src/main/java/chimhaha/chimcard/game/dto/MyGameCardDto.java
+++ src/main/java/chimhaha/chimcard/game/dto/MyGameCardDto.java
@@ -4,8 +4,8 @@ import chimhaha.chimcard.entity.AttackType;
 import chimhaha.chimcard.entity.Card;
 import chimhaha.chimcard.entity.Grade;
 
-public record MyGameCardDto(Long gameCardId, Long cardId, String title, AttackType attackType, Grade grade, int power, String imageUrl) {
+public record MyGameCardDto(Long gameCardId, Long cardId, String title, String attackType, Grade grade, int power, String imageUrl) {
     public MyGameCardDto(Long gameCardId, Card card) {
-        this(gameCardId, card.getId(), card.getTitle(), card.getAttackType(), card.getGrade(), card.getPower(), card.getImageUrl());
+        this(gameCardId, card.getId(), card.getTitle(), card.getAttackType().getType(), card.getGrade(), card.getPower(), card.getImageUrl());
     }
 }
