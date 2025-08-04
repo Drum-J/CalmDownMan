95357752025ec031093f500249100a5f8f06eef2
DrumJ <tmdgh717@naver.com>
2025-08-04 10:32:40 +0900

    add. 유저의 역할 role 추가

--- src/main/java/chimhaha/chimcard/user/dto/UserDetailDto.java
+++ src/main/java/chimhaha/chimcard/user/dto/UserDetailDto.java
@@ -4,12 +4,12 @@ import chimhaha.chimcard.entity.Account;
 
 public record UserDetailDto(Long id, String username, String nickname,
                             String profileImage, Integer point, Integer rankScore,
-                            int winCount, int loseCount, int drawCount
+                            int winCount, int loseCount, int drawCount, String role
 ) {
     public UserDetailDto(Account account) {
         this(account.getId(), account.getUsername(), account.getNickname(),
                 account.getProfileImage(), account.getPoint(), account.getRankScore(),
-                account.getWin(), account.getLose(), account.getDraw()
+                account.getWin(), account.getLose(), account.getDraw(), account.getRole().name()
         );
     }
 }
