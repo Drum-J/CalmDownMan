36d3bf51ee91f2b060ae35e55dbeb83d40a3dbd5
DrumJ <tmdgh717@naver.com>
2025-08-05 14:32:01 +0900

    fix. 웹소켓 테스트 Authorization 헤더 제거 및 Config 변경으로 인한 코드 추가. 구독 - 메세지 발송 사이 대기 추가

--- src/test/java/chimhaha/chimcard/message/WebSocketTest.java
+++ src/test/java/chimhaha/chimcard/message/WebSocketTest.java
@@ -8,15 +8,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.stomp.StompFrameHandler;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
-import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.messaging.simp.stomp.*;
+import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -33,14 +35,20 @@ public class WebSocketTest {
     @Autowired SimpMessagingTemplate messagingTemplate;
 
     // 비동기로 도착하는 메시지를 기다리기 위한 장치
-    private final CompletableFuture<SimpleMessageDto> messageFuture = new CompletableFuture<>();
+    private CompletableFuture<SimpleMessageDto> messageFuture;
     private WebSocketStompClient stompClient;
 
     @BeforeEach
     void setUp() {
-        stompClient = new WebSocketStompClient(new StandardWebSocketClient());
+        //WebSocketConfig.registerStompEndpoints 에 withSockJS 추가로인해 변경
+        List<Transport> transports = new ArrayList<>();
+        transports.add(new WebSocketTransport(new StandardWebSocketClient()));
+        WebSocketClient webSocketClient = new SockJsClient(transports);
+
+        stompClient = new WebSocketStompClient(webSocketClient);
         // 서버와 동일하게 Jackson을 사용하여 JSON 메시지를 객체로 변환하도록 설정
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        messageFuture = new CompletableFuture<>();
     }
 
     @Test
@@ -48,18 +56,14 @@ public class WebSocketTest {
     void messageTest() throws Exception {
         //given
         //JwtProviderTest 에서 생성해서 가져옴.
-        String token = "eyJhbGciOiJIUzUxMiJ9.eyJpZCI6MSwibmlja25hbWUiOiJ0ZXN0VXNlciIsInJvbGUiOiJBRE1JTiIsImlzcyI6Iijso7wp6riI67OR7JiBIiwiaWF0IjoxNzUyNzI5MzE1LCJleHAiOjE3NTI3NjUzMTV9.mDGRyv1qStaQ0IEEkPG5bpOY-FpaNGuIQw65DqCSx4JkEJO4YPE7yNVp_5kU4L1aCkGE5rhrzqYQNe2Oid9m1g";
         String topic = "/topic/messageTest";
         String connectUrl = String.format("ws://localhost:%d/ws-connection", port); // 접속 주소 ws 프로토콜 사용
         SimpleMessageDto messageDto = new SimpleMessageDto("Tester", "Test Message");
 
-        WebSocketHttpHeaders httpHeaders = new WebSocketHttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + token);
-
         //when
         // 1. client 와 server를 연결
         StompSession stompSession = stompClient.connectAsync(
-                connectUrl, httpHeaders, new StompSessionHandlerAdapter() {}
+                connectUrl, new StompSessionHandlerAdapter() {}
         ).get(5, TimeUnit.SECONDS);
 
         // 2. client는 "/topic/messageTest" 을 구독하고 전달받은 메세지를 SimpleMessageDto 객체로 역직렬화.
@@ -75,6 +79,8 @@ public class WebSocketTest {
                 messageFuture.complete((SimpleMessageDto) payload);
             }
         });
+
+        Thread.sleep(1000); // 구독 완료 후 메세지를 보내기 위해 잠시 대기
 
         // 3. server에서 메세지를 보냄. 해당 topic 을 구독중인 client는 메세지를 받을 수 있음.
         messagingTemplate.convertAndSend(topic, messageDto);
