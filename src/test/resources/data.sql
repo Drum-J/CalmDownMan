d76cc21b5bb45e96b0cc2e5afa6e18269165c973
DrumJ <tmdgh717@naver.com>
2025-08-05 15:58:51 +0900

    add. 동시성 문제를 위한 version 추가
    user 패키지 테스트 완료

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,3 @@
 -- 테스트용 유저
-INSERT INTO account (id, username, password, nickname, point, role, win, lose, draw)
-VALUES (1, 'seungho', 'testPassword', '승호', 0, 'USER', 0, 0, 0);
+INSERT INTO account (id, username, password, nickname, point, role, win, lose, draw, version)
+VALUES (1, 'seungho', 'testPassword', '승호', 0, 'USER', 0, 0, 0, 0);
