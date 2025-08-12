87a382e43596789d8edb4f00e04e2f47da5be51b
DrumJ <tmdgh717@naver.com>
2025-08-12 12:18:46 +0900

    feat. 상대 플레이어 정보 추가

--- src/main/java/chimhaha/chimcard/game/dto/GameInfoDto.java
+++ src/main/java/chimhaha/chimcard/game/dto/GameInfoDto.java
@@ -3,8 +3,7 @@ package chimhaha.chimcard.game.dto;
 import java.util.List;
 import java.util.Map;
 
-public record GameInfoDto(String otherPlayer, List<MyGameCardDto> myCards, Long currentTurnPlayerId,
+public record GameInfoDto(OtherPlayerInfo otherPlayer, List<MyGameCardDto> myCards, Long currentTurnPlayerId,
                           Long player1Id, Long player2Id,
-                          Map<Integer, FieldCardDto> fieldCards
-) {
+                          Map<Integer, FieldCardDto> fieldCards) {
 }
